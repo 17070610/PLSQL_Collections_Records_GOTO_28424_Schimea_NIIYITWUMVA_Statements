006a6f92f987c1a64f27cf855bfc02ed379dfb13
Schimea NIYITWUMVA <147380820+17070610@users.noreply.github.com>
2025-11-25 22:09:04 +0200

    add sample data

---
+++ src/sample_data.sql
@@ -0,0 +1,16 @@
+INSERT INTO products(product_id, product_code, description, qty_on_hand, reorder_level, unit_price)
+VALUES (1, 'PEN-001', 'Ballpoint Pen', 50, 20, 0.50);
+
+INSERT INTO products(product_id, product_code, description, qty_on_hand, reorder_level, unit_price)
+VALUES (2, 'NOTE-002', 'Notebook A5', 10, 15, 2.50);
+
+INSERT INTO products(product_id, product_code, description, qty_on_hand, reorder_level, unit_price)
+VALUES (3, 'ERASER-003', 'Eraser', 5, 10, 0.30);
+
+INSERT INTO products(product_id, product_code, description, qty_on_hand, reorder_level, unit_price)
+VALUES (4, 'PENCIL-004', 'HB Pencil', 100, 30, 0.20);
+
+INSERT INTO products(product_id, product_code, description, qty_on_hand, reorder_level, unit_price)
+VALUES (5, 'HIGHL-005', 'Highlighter', 2, 10, 1.00);
+
+COMMIT;
